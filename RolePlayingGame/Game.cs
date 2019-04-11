f77928e998efb961ed0eb4140f21ba4871d1f6df
RJ <rjaggarwal1993@gmail.com>
2019-04-11 22:57:54 +0100

    Comment updates on main methods

--- RolePlayingGame/Game.cs
+++ RolePlayingGame/Game.cs
@@ -76,6 +76,7 @@ namespace RolePlayingGame
             this.player.ReportChosenClass();
         }
 
+        // This method needs work before implementing
         private void ChooseAnotherClass()
         {
 
